df2cd43b6e59b3ec908747fce132fda1cafc36ed
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2017-04-13 23:44:10 +0100

    Update attackfunction.c

--- attackfunction.c
+++ attackfunction.c
@@ -14,44 +14,44 @@ scanf("%d",&attackType);
 
 printf("\nEnter the name of the player you wish to attack out of the following list"); 
 
-for (i= 0; i < playerNumber; i++)
+for (i= 1; i < playerNumber; i++)
 {
 printf("Do you want to attack player %d\n",i);
 }
 
 scanf("%d",&defenderName);
 
-a = player[currPlayer].row;
+a = player[currPlayer].row;  
 b = player[currPlayer].column;
 
 c = player[defenderName].row;
 d = player[defenderName].column;
 
-z=abs(a-b);
-z=z+abs(c-d);
+z=abs(c-a);
+z=z+abs(d-b);
 
-if (attackType == 0 && z >=1 ) {                   // only performed on player on the same slot or 1 away.
+if (attackType == 0 && z <=1 ) {                   // only performed on player on the same slot or 1 away.
 	printf("You have choosen near attack"); 
 	
 	
-	       if(player[currPlayer].strength <= 70)   // if the attacker strength points is less than or equal to 70
+	       if(player[defenderName].strength <= 70)   // if the attacker strength points is less than or equal to 70
 		   {			   
 		       player[defenderName].lifePoints -= 0.5 * player[currPlayer].strength; // reduce defender lifePoints
 	          }
-	             else if(player[currPlayer].strength > 70)  // if the attacker points is greater than 70
+	             else if(player[defenderName].strength > 70)  // if the attacker points is greater than 70
 				 {
 		           player[defenderName].lifePoints -= 0.3 * player[currPlayer].strength; // reduce defender lifePoints
                  }
          }
 
 		 
-		 else if (attackType==0 && z<1)
+		 else if (attackType==0 && z>1)
 		 {
 			   printf("The player must be on the same slot or one slot away for a near attack");
 		 }
 
 		 
-if(attackType == 1 && 1 < z > 5) // only performed against players who are a distance greater 2-4 away
+if(attackType == 1 && 1 < z < 5) // only performed against players who are a distance greater 2-4 away
         {  
 	    printf("You have choosen distant attack");
 	
@@ -64,7 +64,7 @@ if(attackType == 1 && 1 < z > 5) // only performed against players who are a dis
 				 player[defenderName].lifePoints = player[defenderName].lifePoints;
              }
          }
-		 else if (attackType ==1 && z>=1 || z<=5)
+		 else if (attackType ==1 && z<=1 || z>=5)
 			 printf("The player must be 2-4 slots away in order to perform a distant attack");
 
 if(attackType == 2 && player[currPlayer].smartness + player[currPlayer].magicSkills >150){
