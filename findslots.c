d1f5483aca6ad3c4aa4da5ea1a2826ebcd7b8dee
Lorcan1 <lorcan.rooney@ucdconnect.ie>
2017-04-13 14:20:26 +0100

    Add files via upload
    From lecture notes

---
+++ findslots.c
@@ -0,0 +1,43 @@
+void findSlots(int reqDist, int currDist,  struct slot * currSlot, struct slot * foundSlots, int * count,  bool explored[BOARD_SIZE][BOARD_SIZE]){
+
+
+
+	//The base case: the current slot is at the required distance from the starting slot
+	if(currDist == reqDist){
+		//the current slot was not explored
+		if(explored[currSlot->row][currSlot->column] == false){
+			//The next availbale position (indicated by count) at foundSlots points to the current slot
+			*(foundSlots + *count) = *currSlot;
+			//the counter is incremented
+			(*count)++;
+			//the matrix of the explored slots set to true the element at the row and column of the current slot
+			explored[currSlot->row][currSlot->column] = true;
+		}
+	}
+	//The recursive call: the current slot is at a distance that is less than the required distance (more slots still have to be traversed)
+	else{
+		//if the current slot has the up slot != NULL (i.e. the slot is not in the first row)
+		if(currSlot->up != NULL){
+			//invokes function find slots incrementing the current Distance (currDist) and setting the current slot to the up slot
+			findSlots(reqDist, currDist +1,  currSlot->up, foundSlots, count, explored);
+		}
+		//if the current slot has the right slot != NULL (i.e. the slot is not in the last column)
+		if(currSlot->right != NULL){
+			//invokes function find slots incrementing the current Distance (currDist) and setting the current slot to the right slot
+			findSlots(reqDist, currDist +1,  currSlot->right, foundSlots, count, explored);
+		}
+		//if the current slot has the down slot != NULL (i.e. the slot is not in the last row)
+		if(currSlot->down != NULL){
+			//invokes function find slots incrementing the current Distance (currDist) and setting the current slot to the down slot
+			findSlots(reqDist, currDist +1,  currSlot->down, foundSlots, count, explored);
+		}
+		//if the current slot has the left slot != NULL (i.e. the slot is not in the first column)
+		if(currSlot->left != NULL){
+			//invokes function find slots incrementing the current Distance (currDist) and setting the current slot to the left slot
+			findSlots(reqDist, currDist +1,  currSlot->left, foundSlots, count, explored);
+		}
+
+	}
+
+
+}
